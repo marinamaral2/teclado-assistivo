825a89d5890cca98c7de174840e79980a6100f7d
marinamaral2 <155557787+marinamaral2@users.noreply.github.com>
2024-11-02 15:34:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,29 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('teclado-cache').then((cache) => {
-      return cache.addAll([
-        './',
-        './teclado-assistivo.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
-  );
+const CACHE_NAME = 'teclado-assistivo-v1';
+const urlsToCache = [
+    'index.html',
+    'manifest.json',
+    'icon-192.png',
+    'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache aberto');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
